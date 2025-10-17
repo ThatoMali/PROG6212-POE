88b20d31e89ada7b6f6fbfce7e9a2070457c2de5
ThatoMali <ST10447949@imconnect.edu.za>
2025-10-17 15:42:24 +0200

    commit#6
    changes to IClaimService.cs

--- PROG6212 POE/Services/IClaimService.cs
+++ PROG6212 POE/Services/IClaimService.cs
@@ -1,12 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using PROG6212_POE.Models;
+using PROG6212_POE.Models.Data.Entities;
 
 namespace PROG6212_POE.Services
 {
-    public class IClaimService : Controller
+    public interface IClaimService
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        Task<List<Claim>> GetAllClaimsAsync();
+        Task<List<Claim>> GetClaimsByLecturerAsync(int lecturerId);
+        Task<List<Claim>> GetPendingClaimsAsync();
+        Task<Claim> GetClaimByIdAsync(int id);
+        Task<Claim> CreateClaimAsync(ClaimViewModel model, int lecturerId);
+        Task<bool> UpdateClaimStatusAsync(int claimId, string status, int approvedById);
+        Task<bool> DeleteClaimAsync(int id);
+        Task<bool> UploadDocumentAsync(int claimId, IFormFile file);
+        Task<Document> GetDocumentAsync(int claimId);
     }
 }
